a0539281b23cc091d1fd2f3e24f9d1acd70db55c
Erik Egeland Steinnes <erik@maxit-as.com>
2022-08-28 01:13:13 +0200

    added different solutions

---
+++ Python/6Kyu/Find the unique number.py
@@ -0,0 +1,12 @@
+def find_uniq(arr):
+    arr.sort()
+    return(arr[-1] if arr[0] == arr[1] else arr[0])
+
+##alternative solution without list comprehension
+def find_uniq(arr):
+    arr.sort()
+    if arr[0]==arr[1]:
+        return arr[-1]
+    else:
+        return arr[0]
+   
