59240b53b03ea731a5f59064479ae08b734f8333
Erik Egeland Steinnes <erik.1@hotmail.no>
2022-08-28 11:14:53 +0200

    added another 8Kyu kata

---
+++ Python/8Kyu/Convert boolean value to strings.py
@@ -0,0 +1,11 @@
+def bool_to_word(boolean):
+    return("Yes" if boolean == True else "No")
+
+
+
+### alternative
+def bool_to_word(boolean):
+    if boolean == True:
+        return "Yes"
+    else:
+        return "No"
